8dea456a1c6e265d2a76b333ff37228e636be3b3
kyoroma <138451440+kyoroma@users.noreply.github.com>
2023-08-02 12:38:27 +0000

    [Update]4-20

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for  :users 
   
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
   
   patch '/users/:id', to: 'users#update', as: :update_user
